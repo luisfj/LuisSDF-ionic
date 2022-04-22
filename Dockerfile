ceb58b18b28ef18d92b7338ab4473b2884534b57
Luis Fernando Johann <luisfj_pr@hotmail.com>
2022-04-22 15:14:23 -0300

    ajuste das portas

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ FROM nginx:alpine
 COPY default.conf /etc/nginx/conf.d/default.conf
 RUN rm -rf /usr/share/nginx/html/*
 COPY --from=build /app/www/ /usr/share/nginx/html/
-EXPOSE 80
+EXPOSE 8149
--- src/app/_config/api.config.ts
+++ src/app/_config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = { 
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:8049",
     gitUrlBackend: "https://github.com/luisfj/LuisSDF-back",
     gitUrlFrontend: "https://github.com/luisfj/LuisSDF-ionic",
 }
